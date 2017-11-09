48d443a90ddf54978c989561c9344018c56990d6
evelinakuusinen <evelinakuusinen@EvelinasMacbook>
2017-11-09 14:43:43 +0200

    Created game logic

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
 var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cardsInPlay = [] ;
+
+//check for a match
+var checkForMatch = function() {
+	
 if (cardsInPlay[0] === cardsInPlay[1]) {
-alert("You found a match!");
-} else {
-alert("Sorry, try again");
+alert("You found a match!") } 
+else {
+alert("Sorry, try again") }
 }
+
+//for flipping the card and storing
+
+var flipCard = function (cardId) {
+cardsInPlay.push(cards[cardId]);
+console.log("User flipped " + cards[cardId]);
+
+//find number of cards in played
+if (cardsInPlay.length === 2) {
+checkForMatch();
+}
+}
+
+flipCard(0);
+flipCard(2);
+
